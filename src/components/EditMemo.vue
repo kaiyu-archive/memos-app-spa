4232a08ecb9e20a8dc308c1d07ba5e5928b6be7d
kaiyu-tech <kaiyu326@gmail.com>
2021-09-12 08:50:21 +0900

    「Vue CLIを使ってメモアプリをSPAとして作る」の提出物

---
+++ src/components/EditMemo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="edit">
+    内容 <textarea class="new" v-model="selectedMemo.body"></textarea>
+    <button class="edit" v-on:click="saveMemo(selectedMemo)">
+      編集
+    </button>
+    <button class="edit" v-on:click="unselectMemo()">
+      キャンセル
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditMemo',
+  props: [ 'selectedMemo' ],
+
+  methods: {
+    saveMemo: function(memo) {
+      this.$emit('saveMemo', memo)
+    },
+    unselectMemo: function() {
+      this.$emit('unselectMemo')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
